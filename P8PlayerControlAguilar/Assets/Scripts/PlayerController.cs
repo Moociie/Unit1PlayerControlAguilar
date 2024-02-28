13b04e9e5fedeba62730859f22e660596e7ddf39
Kimberly Aguilar <757379@student.dusd.net>
2024-02-28 15:26:49 -0800

    started adding player movement
    started to add vehicle movement for the player

--- P8PlayerControlAguilar/Assets/Scripts/PlayerController.cs
+++ P8PlayerControlAguilar/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 20f;
+    public float turnSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Move the vehicle forward
-        transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
