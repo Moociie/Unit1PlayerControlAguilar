85e2d236ee09e9435ff8bb391f2c02960a1447dd
Kimberly Aguilar <757379@student.dusd.net>
2024-03-01 14:39:17 -0800

    finished Unit 1 player control project
    finished adding the challenges to the game

--- P8PlayerControlAguilar/Assets/Scripts/PlayerController.cs
+++ P8PlayerControlAguilar/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
-    public float turnSpeed;
+    private float speed = 30.0f;
+    private float turnSpeed = 50.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +22,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input
+        
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput *  Time.deltaTime);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
